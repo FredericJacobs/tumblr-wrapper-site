f8cd0b01af9e2e0f25aa56d8d222f6bade42aa0f
Frederic Jacobs <frederic.jacobs@mac.com>
2012-11-11 17:09:10 +0100

    Hellos

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-DEPLOYMENT_MODE = 'dev'
+DEPLOYMENT_MODE = 'prod'
 COMPRESS_REVISION_NUMBER = '1.0'
 
 
@@ -114,6 +114,6 @@ if DEPLOYMENT_MODE == 'dev':
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = '[ENTER PROD URL HERE] ex. http://rigoneri.herokuapp.com/'
+    SITE_ROOT_URI = 'http://www.fredericjacobs.com'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
